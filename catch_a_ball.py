3899ab22a3b42fec7ce0e1d925f7183f23e8d43c
Biklogan13 <krylovski.ii@phystech.edu>
2021-10-11 12:21:49 +0300

    added multiple balls

--- catch_a_ball.py
+++ catch_a_ball.py
@@ -6,6 +6,7 @@ pygame.init()
 pygame.font.init()
 
 FPS = 60
+number = 20
 screen = pygame.display.set_mode((1500, 1000))
 
 RED = (255, 0, 0)
@@ -18,18 +19,17 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-def new_ball(color):
-    circle(screen, color, (x, y), r)
+def new_ball(k):
+    circle(screen, color[k], (x[k], y[k]), r[k])
 
 
-def is_hit(x_mouse, y_mouse):
-    if (x - x_mouse)**2 + (y - y_mouse)**2 <= r**2:
+def is_hit(x_mouse, y_mouse, k):
+    if (x[k] - x_mouse)**2 + (y[k] - y_mouse)**2 <= r[k]**2:
         return True
     else:
         return False
 
 def point_draw():
-    global points
     font1 = pygame.font.SysFont('arial', 100)
     text = str(points)
     tekst = font1.render(text, True, WHITE)
@@ -38,18 +38,26 @@ def point_draw():
 clock = pygame.time.Clock()
 finished = False
 
-
 a = 0
 b = 0
 points = 0
-x = 750
-y = 500
-r = 50
-Vx = 0
-Vy = 0
-color = COLORS[randint(0, 5)]
-new_ball(color)
+hitreg = [0]*number
+x = [750]*number
+y = [500]*number
+r = [50]*number
+Vx = [0]*number
+Vy = [0]*number
+color = [COLORS[randint(0, 5)]]*number
+new_ball(1)
 pygame.display.update()
+
+for i in range(number):
+    x[i] = randint(100, 1400)
+    y[i] = randint(100, 900)
+    r[i] = randint(20, 80)
+    Vx[i] = ((random()-0.5)/0.5)*10
+    Vy[i] = ((random()-0.5)/0.5)*10
+    color[i] = COLORS[randint(0, 5)]
 
 while not finished:
     clock.tick(FPS)
@@ -61,26 +69,37 @@ while not finished:
                 a = event.pos[0]
                 b = event.pos[1]
 
-    if x <= r or x >= 1500-r: Vx = -Vx
-    if y <= r or y >= 1000-r: Vy = -Vy
+    for i in range(number):
+        if x[i] <= r[i] or x[i] >= 1500 - r[i]: Vx[i] = -Vx[i]
+        if y[i] <= r[i] or y[i] >= 1000 - r[i]: Vy[i] = -Vy[i]
 
-    new_ball(color)
-    x += Vx
-    y += Vy
+    for i in range(number):
+        new_ball(i)
+        x[i] += Vx[i]
+        y[i] += Vy[i]
+
     screen.fill(BLACK)
-    new_ball(color)
 
-    if is_hit(a, b):
-        x = randint(100, 1100)
-        y = randint(100, 900)
-        r = randint(10, 100)
-        Vx = ((random()-0.5)/0.5)*10
-        Vy = ((random()-0.5)/0.5)*10
-        color = COLORS[randint(0, 5)]
-        points += 1
-        new_ball(color)
-        screen.fill(BLACK)
-        pygame.display.update()
+    for i in range(number):
+        new_ball(i)
+
+    for i in range(number):
+        if is_hit(a, b, i):
+            hitreg[i] = 1
+        else: hitreg[i] = 0
+
+    for i in range(number):
+        if hitreg[i] == 1:
+            x[i] = randint(100, 1400)
+            y[i] = randint(100, 900)
+            r[i] = randint(20, 80)
+            Vx[i] = ((random()-0.5)/0.5)*10
+            Vy[i] = ((random()-0.5)/0.5)*10
+            color[i] = COLORS[randint(0, 5)]
+            points += 1
+            new_ball(i)
+            screen.fill(BLACK)
+            pygame.display.update()
 
     point_draw()
     print(x, y, r)
